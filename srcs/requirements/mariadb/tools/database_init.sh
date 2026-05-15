6d1d207ae3c45108f5736eb1e843ddfbe75ff5a4
jguelen <jguelen@student.42lehavre.fr>
2026-05-15 18:56:19 +0200

    testing

--- srcs/requirements/mariadb/tools/database_init.sh
+++ srcs/requirements/mariadb/tools/database_init.sh
@@ -2,7 +2,7 @@
 
 for i in {1..10}; do
     sleep 3
-    mariadb < init.sql && exit 0
+    mariadb < init.sql && printf "Successfully initiated database\n" && exit 0
 done
 
 printf "Failed to create and init the Database\n"
--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -5,14 +5,13 @@ set -eux
 chown -R mysql:mysql /var/log/mysql
 chown -R mysql:mysql /run/mysqld
 
-if [ -f mariadb_initialized ] 
+if [ -d "/var/lib/mysql/mysql" ] 
 then
-    printf "Database already exists\n"
+    printf "Database already exists. Skipping setup.\n"
 else
     printf "Installing and initializing database\n"
     mariadb-install-db 
     ./database_init.sh&
-    echo "Done" > mariadb_initialized
 fi
 
 exec "$@"
