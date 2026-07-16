11c79dcad5d4452a544c84f2011b440474eb64c8
jguelen <jguelen@student.42lehavre.fr>
2026-07-16 19:02:44 +0200

    feat FTP v1.0

--- srcs/requirements/bonus/FTP/tools/entrypoint.sh
+++ srcs/requirements/bonus/FTP/tools/entrypoint.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 
+printf "Launching FTP server..."
+
+FTP_USER=$(cat /run/secrets/ftp_credentials | sed -n 1p)
+FTP_PASSWORD=$(cat /run/secrets/ftp_credentials | sed -n 2p)
+
+# Create user if missing
+if ! id "$FTP_USER" >/dev/null 2>&1; then
+  useradd -m -d /var/www/html -s /bin/bash "$FTP_USER"
+fi
+
+echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
+
+mkdir -p /etc/vsftpd
+
+grep -qxF "$FTP_USER" || echo "$FTP_USER" >> /etc/vsftpd/user_list
+
+mkdir -p /etc/ssl/private
+if [ ! -f /etc/ssl/private/vsftpd.pem ]; then
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/vsftpd.pem \
+    -out /etc/ssl/private/vsftpd.pem \
+    -subj "/C=FR/ST=Normandy/L=Le Havre/O=42/CN=jguelen.42.fr"
+fi
+
+exec "$@"
