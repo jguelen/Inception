7101977fd8b6a69da960d867e2643eb3a4ca676f
jguelen <jguelen@student.42lehavre.fr>
2026-07-24 19:16:49 +0200

    WIP: ftp sefaults, save to verify if the problem is the host machine

--- srcs/requirements/bonus/FTP/tools/entrypoint.sh
+++ srcs/requirements/bonus/FTP/tools/entrypoint.sh
@@ -1,30 +1,55 @@
+## #!/bin/bash
+## 
+## printf "Launching FTP server..."
+## 
+## FTP_USER=$(cat /run/secrets/ftp_credentials | sed -n 1p | tr -d '\r')
+## FTP_PASSWORD=$(cat /run/secrets/ftp_credentials | sed -n 2p | tr -d '\r')
+## 
+## # Create user if missing
+## if ! id "$FTP_USER" >/dev/null 2>&1; then
+##   useradd -m -d /var/www/html -s /bin/bash "$FTP_USER"
+## fi
+## 
+## echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
+## 
+## mkdir -p /etc/vsftpd
+## 
+## touch /etc/vsftpd/user_list
+## 
+## grep -qxF "$FTP_USER" /etc/vsftpd/user_list || echo "$FTP_USER" >> /etc/vsftpd/user_list
+## 
+## mkdir -p /etc/ssl/private
+## if [ ! -f /etc/ssl/private/vsftpd.pem ]; then
+##   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+##     -keyout /etc/ssl/private/vsftpd.pem \
+##     -out /etc/ssl/private/vsftpd.pem \
+##     -subj "/C=FR/ST=Normandy/L=Le Havre/O=42/CN=jguelen.42.fr"
+## fi
+## 
+## chmod 600 /etc/ssl/private/vsftpd.pem
+## chown root:root /etc/ssl/private/vsftpd.pem
+## 
+## mkdir -p /var/run/vsftpd/empty
+## 
+## chown -R "$FTP_USER:$FTP_USER" /var/www/html
+## chmod -R 775 /var/www/html
+## 
+## exec "$@"
+
 #!/bin/bash
+set -e
+FTP_USER=$(cat /run/secrets/ftp_credentials | sed -n 1p | tr -d '\r')
+FTP_PASSWORD=$(cat /run/secrets/ftp_credentials | sed -n 2p | tr -d '\r')
 
-printf "Launching FTP server..."
-
-FTP_USER=$(cat /run/secrets/ftp_credentials | sed -n 1p)
-FTP_PASSWORD=$(cat /run/secrets/ftp_credentials | sed -n 2p)
-
-# Create user if missing
-if ! id "$FTP_USER" >/dev/null 2>&1; then
-  useradd -m -d /var/www/html -s /bin/bash "$FTP_USER"
+mkdir -p /var/run/vsftpd/empty /var/ftp/upload
+if ! id "${FTP_USER}" &>/dev/null; then
+  useradd -d /var/ftp -s /bin/bash "${FTP_USER}"
 fi
-
 echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
-
-mkdir -p /etc/vsftpd
-
-touch /etc/vsftpd/user_list
-
-grep -qxF "$FTP_USER" /etc/vsftpd/user_list || echo "$FTP_USER" >> /etc/vsftpd/user_list
-
-mkdir -p /etc/ssl/private
-if [ ! -f /etc/ssl/private/vsftpd.pem ]; then
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/private/vsftpd.pem \
-    -out /etc/ssl/private/vsftpd.pem \
-    -subj "/C=FR/ST=Normandy/L=Le Havre/O=42/CN=jguelen.42.fr"
-    chmod 600 /etc/ssl/private/vsftpd.pem
-fi
+chown -R "${FTP_USER}:${FTP_USER}" /var/ftp
+chmod 755 /var/ftp
+chmod 755 /var/ftp/upload
+echo "${FTP_USER}" > /etc/vsftpd.userlist
+touch /var/log/vsftpd.log
 
 exec "$@"
