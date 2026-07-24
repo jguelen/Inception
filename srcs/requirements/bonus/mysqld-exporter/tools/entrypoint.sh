c1d415577a3c0cc58c81e29da2ca507198841461
jguelen <jguelen@student.42lehavre.fr>
2026-07-24 13:15:23 +0200

    fix+feat: fixed config for mysqld-expoter + included premade dashboards for grafana

--- srcs/requirements/bonus/mysqld-exporter/tools/entrypoint.sh
+++ srcs/requirements/bonus/mysqld-exporter/tools/entrypoint.sh
@@ -4,15 +4,15 @@ set -e
 
 printf "Launching mysqld-exporter\n"
 
-USER=$(cat /run/secrets/db_ro_credentials | sed -n 1p)
-PASSWORD=$(cat /run/secrets/db_ro_credentials | sed -n 2p)
+USER=$(cat /run/secrets/db_ro_credentials | sed -n 1p | tr -d '\r')
+PASSWORD=$(cat /run/secrets/db_ro_credentials | sed -n 2p | tr -d '\r')
 
 mkdir -p /etc/mysqld-exporter/
 
 cat >/etc/mysqld-exporter/.my.cnf <<EOF
 [client]
-user=${USER}
-password=${PASSWORD}
+user=$USER
+password=$PASSWORD
 host=mariadb
 port=3306
 EOF
